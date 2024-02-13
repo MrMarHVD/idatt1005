53f71ffbec5396ef26ca55e87bc5d766d8ddb53e
Håvard Daleng <142524365+MrMarHVD@users.noreply.github.com>
2024-02-13 14:52:47 +0100

    Reorganized interface to use a BorderPane for convenience and added toolbar controller.

---
+++ src/main/java/no/ntnu/idatt1005/plate/controller/UiInventoryController.java
@@ -0,0 +1,15 @@
+package no.ntnu.idatt1005.plate.controller;
+
+import javafx.geometry.Insets;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import no.ntnu.idatt1005.plate.controller.calendar.CalendarController;
+import no.ntnu.idatt1005.plate.model.Recipe;
+import no.ntnu.idatt1005.plate.model.CookbookMaker;
+
+public class UiInventoryController {
+
+}
