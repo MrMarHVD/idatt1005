edd1dc1bc93267423733dfc622f8b183d52e66ae
Håvard Daleng <142524365+MrMarHVD@users.noreply.github.com>
2024-02-13 11:32:33 +0100

    Improved UI and added some more fxml-files and corresponding controllers.

---
+++ src/main/java/no/ntnu/idatt1005/plate/controller/calendar/CalendarController.java
@@ -0,0 +1,38 @@
+package no.ntnu.idatt1005.plate.controller.calendar;
+
+import javafx.geometry.Insets;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javax.swing.text.html.ImageView;
+import no.ntnu.idatt1005.plate.model.Recipe;
+import no.ntnu.idatt1005.plate.model.CookbookMaker;
+
+public class CalendarController {
+
+  @FXML
+  private DayBlockController mondayController;
+
+  @FXML
+  private DayBlockController tuesdayController;
+
+  @FXML
+  private DayBlockController wednesdayController;
+
+  @FXML
+  private DayBlockController thursdayController;
+
+  @FXML
+  private DayBlockController fridayController;
+
+  @FXML
+  private DayBlockController saturdayController;
+
+  @FXML
+  private DayBlockController sundayController;
+
+
+
+
+}
