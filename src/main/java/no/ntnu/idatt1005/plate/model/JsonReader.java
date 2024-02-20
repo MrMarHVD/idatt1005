44c60d293b1a7be98466794f9f2e8c51e9fd8434
Nikolai Tandberg <nikoltan@stud.ntnu.no>
2024-02-20 12:09:15 +0100

    feat: add JsonReader class for reading objects from JSON files

---
+++ src/main/java/no/ntnu/idatt1005/plate/model/JsonReader.java
@@ -0,0 +1,84 @@
+package no.ntnu.idatt1005.plate.model;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+/**
+* Class for reading objects from JSON files.
+*/
+public class JsonReader {
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+
+  private JsonReader() {
+  }
+
+  /**
+   * Reads a list of ingredients from a JSON file.
+   *
+   *
+   * @param filePath path to the JSON file.
+   * @param typeReference type reference for the list of ingredients.
+   * @return list of ingredients.
+   */
+  private static <T> List<T> readValueFromFile(
+          String filePath,
+          TypeReference<List<T>> typeReference
+  ) {
+    try {
+      return objectMapper.readValue(new File(filePath), typeReference);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return Collections.emptyList();
+    }
+  }
+
+  /**
+   * Gets a single ingredient by its id.
+   *
+   *
+   * @return ingredient with the given id, or null if not found.
+   */
+  public static Ingredient getIngredientById(int id) {
+    List<Ingredient> ingredients = readValueFromFile(
+            "src/main/resources/ingredients.json",
+            new TypeReference<>() {}
+    );
+    return ingredients.stream().filter(ingredient -> ingredient.getId() == id)
+            .findFirst().orElse(null);
+  }
+
+  /**
+   * Gets a single recipe by its id.
+   *
+   *
+   * @return recipe with the given id, or null if not found.
+   */
+  public static Recipe getRecipeById(int id) {
+    List<Recipe> recipes = readValueFromFile(
+            "src/main/resources/recipes.json",
+            new TypeReference<>() {}
+    );
+    return recipes.stream().filter(recipe -> recipe.getId() == id).findFirst().orElse(null);
+  }
+
+  /**
+   * Gets a single cookbook by its id.
+   *
+   *
+   * @return cookbook with the given id, or null if not found.
+   */
+  public static CookBook getCookBookById(int id) {
+    List<CookBook> cookBooks = readValueFromFile(
+            "src/main/resources/cookbooks.json",
+            new TypeReference<>() {}
+    );
+    return cookBooks.stream().filter(cookBook -> cookBook.getId() == id).findFirst().orElse(null);
+  }
+
+
+
+}
